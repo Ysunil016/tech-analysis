5440c074908df45c35038db19b8d662a01adef8d
Yadav, Sunil <atworksunil@gmail.com>
2023-05-21 09:56:42 +0530

    add details to project

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,19 @@
 import { typescript } from 'projen';
+import { NpmAccess } from 'projen/lib/javascript';
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   name: 'tech-analysis',
   projenrcTs: true,
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  authorName: 'Yadav, Sunil',
+  authorEmail: 'atworksunil@gmail.com',
+  authorUrl: 'https://ysunil016.github.io/',
+  packageName: '@sa-tools/tech-analysis',
+  deps: [], /* Runtime dependencies of this module. */
+  description: 'Technical Analysis, for Candles', /* The description is just a string that helps people understand the purpose of the package. */
+  devDeps: [], /* Build dependencies for this module. */
+  homepage: '',
+  npmAccess: NpmAccess.PUBLIC,
+  repository: '',
 });
+
 project.synth();
